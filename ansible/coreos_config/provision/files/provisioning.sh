5032a5ddd0d1ee617822f4f522620798f859a65a
James Miller <jamesstewartmiller@gmail.com>
2023-03-30 17:29:15 +0100

    testing

---
+++ ansible/coreos_config/provision/files/provisioning.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+is_mounted() {
+    mount | awk -v DIR="$1" '{if ($3 == DIR) { exit 0}} ENDFILE{exit -1}'
+}
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+[[ -d /tmp/FCOSEFIpart ]] && rm -rf /tmp/FCOSEFIpart
+FCOSDISK=/dev/sda
+sudo coreos-installer install --architecture=aarch64 -i ${SCRIPT_DIR}/config.ign ${FCOSDISK}
+
+FCOSEFIPARTITION=$(lsblk $FCOSDISK -J -oLABEL,PATH  | jq -r '.blockdevices[] | select(.label == "EFI-SYSTEM")'.path)
+mkdir /tmp/FCOSEFIpart
+sudo mount $FCOSEFIPARTITION /tmp/FCOSEFIpart
+pushd /tmp/FCOSEFIpart
+VERSION=$(curl https://api.github.com/repos/pftf/RPi4/releases/latest -s | jq .name -r)  # use latest one from https://github.com/pftf/RPi4/releases
+sudo curl -LO https://github.com/pftf/RPi4/releases/download/${VERSION}/RPi4_UEFI_Firmware_${VERSION}.zip
+sudo unzip RPi4_UEFI_Firmware_${VERSION}.zip
+sudo rm RPi4_UEFI_Firmware_${VERSION}.zip
+popd
+if is_mounted /tmp/FCOSEFIpart; then
+    sudo umount /tmp/FCOSEFIpart
+fi
+find /tmp/ -name "coreos-installer*" -maxdepth 1 -type=d exec sudo rm -rf {} +
+[[ -d /tmp/FCOSEFIpart ]] && sudo rm -rf /tmp/FCOSEFIpart
